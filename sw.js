e34e903146d6383ae261287dd40577d960b799d2
step4progress2025-byte <step4progress2025@gmail.com>
2026-06-10 18:18:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-const CACHE = 's4p-crm-v3';
-const ASSETS = [
-  '/crm/',
-  '/crm/index.html',
-  '/crm/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  .then(() => clients.claim())
+));
+self.addEventListener('fetch', () => {});
